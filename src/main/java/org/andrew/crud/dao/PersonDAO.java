c0af310a2634b1cd22d5d56c0f81c1e2ca07ae7c
Kulbicki Andrey <kubl-ml@mail.ru>
2022-11-03 16:32:49 +0300

    CRUD_App with DAO (local list)

---
+++ src/main/java/org/andrew/crud/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package org.andrew.crud.dao;
+
+import org.andrew.crud.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Jack"));
+        people.add(new Person(++PEOPLE_COUNT, "Fill"));
+        people.add(new Person(++PEOPLE_COUNT, "Sam"));
+        people.add(new Person(++PEOPLE_COUNT, "Lens"));
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
